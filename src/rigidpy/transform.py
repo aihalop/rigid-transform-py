fb740069f9b713b33e5f0c1a58c8af89dc578c1b
Jin Cao <aihalop@gmail.com>
2019-12-30 22:09:05 +0800

    Add angle and vectorize methods for Rigid2D.

--- src/rigidpy/transform.py
+++ src/rigidpy/transform.py
@@ -1,12 +1,21 @@
+# Rigid Transformation objects.
+# 
+# Jin Cao <aihalop@gmail.com>
+# 2019-12-2
+
 import numpy as np
 import unittest
 
 class Quaternion(object):
-    def __init__(self, w=1.0, x=0.0, y=0.0, z=0.0):
-        self._w = w
-        self._x = x
-        self._y = y
-        self._z = z
+    def __init__(self, *args, **kwargs):
+        if args and not kwargs:
+            w, x, y, z = args
+            self._w = w
+            self._x = x
+            self._y = y
+            self._z = z
+        elif not args and kwargs:
+            pass
 
     def scalar(self):
         return self._w
@@ -28,6 +37,9 @@ class Quaternion(object):
 
     def matrix(self):
         pass
+
+    def to_list(self):
+        return np.array([self._w, self._x, self._y, self._z])
 
 
 class Rigid2D(object):
@@ -68,33 +80,52 @@ class Rigid2D(object):
 
     def __str__(self):
         return "({}, {})".format(
-            self.translation(), np.arccos(self._rotation[0, 0])
+            self.translation(),
+            self.angle()
         )
+
+    def angle(self):
+        return np.arctan2(self._rotation[1, 0], self._rotation[0, 0])
+
+    def vectorize(self):
+        return np.concatenate([self.translation(), np.array([self.angle()])])
 
 
 class TestRigid2D(unittest.TestCase):
     def setUp(self):
         self.A = Rigid2D(1, 2, np.pi/6)
         self.B = Rigid2D(3, 3, np.pi/4)
-        print(self.A)
+        self.C = Rigid2D(0, 0, np.pi / 4)
+        print("A = {}, B = {}".format(self.A, self.B))
 
     def tearDown(self):
         pass
 
     def test_inverse(self):
-        print(self.A.inverse() * self.B)
+        print("A.inverse() * B = {}".format(self.A.inverse() * self.B))
+        print("C.inverse() * C = {}".format(self.C.inverse() * self.C))
+        print("C * C.inverse() = {}".format(self.C * self.C.inverse()))
+
+    def test_vectorize(self):
+        print("self.A.vectorize(): ", self.A.vectorize(), type(self.A.vectorize()))
+        self.assertEqual(type(self.A.vectorize()), np.ndarray)
+        np.testing.assert_array_almost_equal(
+            self.A.vectorize(), [1.0, 2.0, 0.5235987]
+        )
 
 
 class TestQuaternion(unittest.TestCase):
     def setUp(self):
-        self.Q = Quaternion(1.0, 2.0, 3.0, 4.0)
-        self.Q2 = Quaternion(2.0, 3.0, 4.0, 5.0)
+        # angle = pi/6, axis = (1., 0., 0.)
+        self.Q = Quaternion(0.9659258262890683, 0.25881904510252074, 0.0, 0.0)
 
     def test_initialization(self):
         print("Q: {}".format(self.Q))
 
     def test_multiple(self):
-        print("Q: {} * Q2: {} = \n{}".format(self.Q, self.Q2, self.Q * self.Q2))
+        q1 = self.Q * self.Q
+        q2 = Quaternion(0.8660254037844387, 0.49999999999999994, 0.0, 0.0)
+        np.testing.assert_array_almost_equal(q1.to_list(), q2.to_list())
 
 
 if __name__=="__main__":
