84d0cf6a03b0960b804e309d2646e97ad5c4d11c
Jin Cao <aihalop@gmail.com>
2022-04-26 21:57:02 +0800

    Change package name.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rigidpy", # Replace with your own username
+    name="rigid-transform-py", # Replace with your own username
     version="0.0.1",
     author="Jin Cao",
     author_email="aihalop@gmail.com",
